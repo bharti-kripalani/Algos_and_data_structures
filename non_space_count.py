22dd6a5bc8959c544799478210fd8da00ff3eb63
bharti-kripalani <kripalani.bharti@gmail.com>
2026-01-03 17:02:16 -0800

    Add algorithm explanation for non-space counting
    Added comments explaining the algorithm for counting spaces in an array.

--- non_space_count.py
+++ non_space_count.py
@@ -1,3 +1,13 @@
+# This algorithm will find out if any location in the array matches a single white space ‘ ‘. It is a very popular interview question during phone interviews. It helps to gauge if the candidate is well versed with the basics and has basic coding skills.
+
+# Algorithm:
+# 1) Initialize count to 0. 
+# 2) Run a for loop from 0 to length of array. 
+# 3) Compare each array location’s character with the space ‘ ‘
+# 4) If it matches then increment the count. 
+# 5) Print the count of spaces in the array.
+
+### IMPLEMENTATION ###
 # Create a list of 10 characters
 # First three are 'a', the rest are space characters
 a = ['a', 'a', 'a', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
